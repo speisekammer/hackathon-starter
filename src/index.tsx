937ab6184503c222fe569140f12539176b62b316
Matthias Schmid <mail@matthias-schmid.de>
2022-08-22 13:08:25 +0200

    chore: remove webvitals

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-import reportWebVitals from './reportWebVitals';
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
@@ -12,7 +10,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
